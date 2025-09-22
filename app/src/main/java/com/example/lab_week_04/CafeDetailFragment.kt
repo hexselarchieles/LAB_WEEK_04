c67f63557e6e80f59af18c797aeecd8269d08344
hexselarchieles <hexselavmm@gmail.com>
2025-09-22 19:06:49 +0700

    Commit No. 3 – Tabbeb Navigation.

---
+++ app/src/main/java/com/example/lab_week_04/CafeDetailFragment.kt
@@ -0,0 +1,23 @@
+package com.example.lab_week_04
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CafeDetailFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate layout (buat file fragment_cafe_detail.xml)
+        return inflater.inflate(R.layout.fragment_cafe_detail, container, false)
+    }
+}
